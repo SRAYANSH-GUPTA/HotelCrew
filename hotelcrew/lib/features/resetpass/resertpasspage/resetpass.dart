ae852f22df303a3b056bf70e6b40afd9590fe2f7
srayansh <sr.gupta621@gmail.com>
2024-10-28 20:34:27 +0530

    created reset pass page

---
+++ hotelcrew/lib/features/resetpass/resertpasspage/resetpass.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Resetpass extends StatefulWidget {
+  const Resetpass({super.key});
+
+  @override
+  State<Resetpass> createState() => _ResetpassState();
+}
+
+class _ResetpassState extends State<Resetpass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
