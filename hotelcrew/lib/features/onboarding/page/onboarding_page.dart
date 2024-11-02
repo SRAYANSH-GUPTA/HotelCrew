0ab3c52a84b0b9608fbc6cc2c003d1e93c793ef0
srayansh <sr.gupta621@gmail.com>
2024-11-02 13:13:27 +0530

    adding carousel slider

--- hotelcrew/lib/features/onboarding/page/onboarding_page.dart
+++ hotelcrew/lib/features/onboarding/page/onboarding_page.dart
@@ -1,18 +1,120 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
 
 class OnboardingPage extends StatefulWidget {
-  const OnboardingPage({super.key});
-
   @override
-  State<OnboardingPage> createState() => _OnboardingPageState();
+  _OnboardingPageState createState() => _OnboardingPageState();
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0, right: 16.0), // Adjust padding as needed
+            child: Align(
+              alignment: Alignment.topRight,
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = imgList.length - 1; // Skip to the last image
+                  });
+                },
+                child: Text(
+                  'Skip',
+                  style: TextStyle(
+                    color: Colors.blue, // Change color as needed
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CarouselSlider.builder(
+              itemCount: imgList.length,
+              itemBuilder: (context, index, realIndex) {
+                return Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(imgList[index]),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
+              options: CarouselOptions(
+                height: MediaQuery.of(context).size.height,
+                viewportFraction: 1.0, // Show only one image at a time
+                aspectRatio: 16 / 9,
+                enlargeCenterPage: false, // Disable center page enlargement
+                autoPlay: false,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((entry) {
+              return GestureDetector(
+                onTap: () => setState(() {
+                  _currentIndex = entry.key;
+                }),
+                child: Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black)
+                        .withOpacity(_currentIndex == entry.key ? 0.9 : 0.4),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = (_currentIndex - 1 + imgList.length) % imgList.length;
+                  });
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = (_currentIndex + 1) % imgList.length;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- hotelcrew/lib/main.dart
+++ hotelcrew/lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Navigator.pushReplacement<void, void>(
     context,
     MaterialPageRoute<void>(
-      builder: (BuildContext context) => const OnboardingPage(),
+      builder: (BuildContext context) => OnboardingPage(),
     ),
     );
   }
