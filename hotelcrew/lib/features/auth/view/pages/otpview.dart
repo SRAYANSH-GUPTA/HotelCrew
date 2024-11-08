228b861981db7633ba07e26c09b21882663989be
srayansh <sr.gupta621@gmail.com>
2024-11-08 08:39:21 +0530

    Added resposiveness to otp page

--- hotelcrew/lib/features/auth/view/pages/otpview.dart
+++ hotelcrew/lib/features/auth/view/pages/otpview.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/services.dart';
 import 'package:hotelcrew/features/auth/view/pages/login_page.dart';
-import 'package:hotelcrew/features/hoteldetails/pages/hoteldetailspage1.dart';
 import 'package:otp_pin_field/otp_pin_field.dart';
 import 'package:otp_timer_button/otp_timer_button.dart';
 import 'otpviewmodel.dart';
@@ -14,16 +13,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:developer';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-
 class Otpview extends StatefulWidget {
   final String email;
   final String password;
   final String confirmpassword;
   final String username;
-   // Assuming you pass the email to this page
 
-  Otpview({required this.email,required this.password,required this.confirmpassword,required this.username});
-
+  Otpview({required this.email, required this.password, required this.confirmpassword, required this.username});
 
   @override
   State<Otpview> createState() => _OtpviewState();
@@ -37,15 +33,16 @@ class _OtpviewState extends State<Otpview> {
   double svgWidth = 232.28;
   bool otperror = false;
   final storage = FlutterSecureStorage();
-   bool _isLoading = false; 
+  bool _isLoading = false;
   final FocusNode emailFocusNode = FocusNode();
   final OtpTimerButtonController otp = OtpTimerButtonController();
-  String enteredOtp = ""; // Variable to store the OTP entered by the user
-bool isLoading = false;
+  String enteredOtp = "";
+  bool isLoading = false;
+
   @override
   void initState() {
     super.initState();
-    viewModel = OtpViewModel(); 
+    viewModel = OtpViewModel();
     emailFocusNode.addListener(() {
       setState(() {
         if (emailFocusNode.hasFocus) {
@@ -71,23 +68,27 @@ bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
+    // Getting screen dimensions for responsiveness
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    double paddingHorizontal = screenWidth * 0.05; // 5% padding on each side
+    double topPadding = screenHeight * 0.07; // 10% padding at the top
+    double otpFieldSize = screenWidth * 0.127; // 20% of the screen width for each OTP field
+
     return GestureDetector(
       onTap: () => _hideKeyboard(),
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16, top: 64),
-            child: SizedBox(
-              width: 328,
-              height: 800,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 328,
-                    height: 32,
-                    child: Text(
+        body: SafeArea(
+          top: true,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: paddingHorizontal, vertical: topPadding),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
                       'Verify Your Account',
                       style: GoogleFonts.montserrat(
                         textStyle: const TextStyle(
@@ -98,11 +99,8 @@ bool isLoading = false;
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 42,
-                    width: 328,
-                    child: Text(
+                    SizedBox(height: screenHeight * 0.02),
+                    Text(
                       "We've sent a 4-digit verification code to your email ${widget.email}",
                       style: GoogleFonts.montserrat(
                         textStyle: const TextStyle(
@@ -113,48 +111,33 @@ bool isLoading = false;
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 48),
-                  Focus(
-                    focusNode: emailFocusNode,
-                    child: Container(
-                      width: 328,
-                      height: 45,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 0),
+                    SizedBox(height: screenHeight * 0.06),
+                    Focus(
+                      focusNode: emailFocusNode,
+                      child: Container(
+                        width: screenWidth * 0.9,
                         child: OtpPinField(
-                          
-                          fieldHeight: 45,
-                          fieldWidth: 45,
+                          fieldHeight: otpFieldSize,
+                          fieldWidth: otpFieldSize,
                           autoFocus: false,
                           key: _otpPinFieldController,
                           autoFillEnable: false,
                           textInputAction: TextInputAction.done,
                           onSubmit: (text) {
-                            enteredOtp = text; // Update enteredOtp when OTP is submitted
+                            enteredOtp = text;
                             debugPrint('Entered pin is $enteredOtp');
                           },
                           onChange: (text) {
-                            enteredOtp = text; // Update enteredOtp on change
+                            enteredOtp = text;
                             debugPrint('Enter on change pin is $enteredOtp');
-                          },
-                          onCodeChanged: (code) {
-                            enteredOtp = code; // Update enteredOtp when code changes
-                            debugPrint('onCodeChanged is $enteredOtp');
                           },
                           otpPinFieldStyle: const OtpPinFieldStyle(
                             activeFieldBackgroundColor: Color(0xFFF4F8F9),
                             filledFieldBackgroundColor: Color(0xFFF4F8F9),
                             fieldBorderWidth: 0,
-
                             fieldPadding: 25.33,
                             activeFieldBorderGradient:
                                 LinearGradient(colors: [Color(0xFF6F8393), Color(0xFF6F8393)]),
-                            filledFieldBorderGradient:
-                                LinearGradient(colors: [Color(0xFF6F8393), Color(0xFF6F8393)]),
-                            defaultFieldBorderGradient:
-                                LinearGradient(colors: [Color(0xFF6F8393), Color(0xFF6F8393)]),
-                          
                           ),
                           maxLength: 4,
                           showCursor: true,
@@ -165,66 +148,63 @@ bool isLoading = false;
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 57),
-                  // Conditional error message when svgHeight is 0
-                  if (svgHeight == 0 && otperror)
-                    Padding(
-                      padding: const EdgeInsets.only(left: 0),
-                      child: SizedBox(
-                        width: 326,
-                        height: 39,
-                        child: Center(
-                          child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                            children: [Text(
-                              "Incorrect code or Invalid User.",
-                              style: GoogleFonts.montserrat(
-                                textStyle: const TextStyle(
-                                  color: Color(0xFFBA4872),
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 14,
+                    SizedBox(height: screenHeight * 0.07),
+                    if (svgHeight == 0 && otperror)
+                      Padding(
+                        padding: const EdgeInsets.only(left: 0),
+                        child: SizedBox(
+                          width: screenWidth * 0.9,
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Incorrect code or Invalid User.",
+                                  style: GoogleFonts.montserrat(
+                                    textStyle: const TextStyle(
+                                      color: Color(0xFFBA4872),
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 14,
+                                    ),
+                                  ),
                                 ),
-                              ),
+                                Text(
+                                  "Check and try again",
+                                  style: GoogleFonts.montserrat(
+                                    textStyle: const TextStyle(
+                                      color: Color(0xFFBA4872),
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                            Text(
-                            "Check and try again",
-                            style: GoogleFonts.montserrat(
-                              textStyle: const TextStyle(
-                                color: Color(0xFFBA4872),
-                                fontWeight: FontWeight.w500,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ),]
                           ),
                         ),
                       ),
+                    Container(
+                      width: screenWidth * 0.9,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
+                        child: SvgPicture.asset(
+                          'assets/otp.svg',
+                          height: svgHeight,
+                          width: svgWidth,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
                     ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 45.98, right: 45.98),
-                    child: SvgPicture.asset(
-                      'assets/otp.svg',
-                      height: svgHeight,
-                      width: svgWidth,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    height: 190,
-                    width: 328,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text("Didn’t receive the code?"),
-                            Container(
-                              width: 108,
-                              height: 40,
-                              padding: EdgeInsets.only(left: 0),
-                              child: OtpTimerButton(
+                    SizedBox(height: screenHeight * 0.03),
+                    Container(
+                      width: screenWidth * 0.9,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text("Didn’t receive the code?"),
+                              OtpTimerButton(
                                 buttonType: ButtonType.text_button,
                                 backgroundColor: Colors.black,
                                 controller: otp,
@@ -278,7 +258,6 @@ setState(() {
   }
 },
 
-
                                 text: Text(
                                   'Resend',
                                   style: GoogleFonts.montserrat(
@@ -292,15 +271,13 @@ setState(() {
                                 ),
                                 duration: 30,
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 16),
-                        SizedBox(
-                          height: 40,
-                          width: 328,
-                          child: ElevatedButton(
-  onPressed: isLoading
+                            ],
+                          ),
+                          SizedBox(height: screenHeight * 0.02),
+                          Container(
+                            width: screenWidth * 0.9,
+                            child: ElevatedButton(
+                               onPressed: isLoading
     ? null
     : () async {
         setState(() {
@@ -373,43 +350,39 @@ setState(() {
           isLoading = false;
         });
       },
-
-  style: ElevatedButton.styleFrom(
-    backgroundColor: const Color(0xFF47518C),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(8),
-    ),
-  ),
-  child: isLoading
-      ? const SizedBox(
-          height: 20,
-          width: 20,
-          child: CircularProgressIndicator(
-            color: Colors.white, // Change color as needed
-            strokeWidth: 2,
-          ),
-        )
-      : Text(
-          'Verify',
-          style: GoogleFonts.montserrat(
-            textStyle: const TextStyle(
-              color: Color(0xFFFAFAFA),
-              fontWeight: FontWeight.w600,
-              fontSize: 14,
-              height: 1.5,
-            ),
-          ),
-        ),
-)
-
-
-                        ),
-                        const SizedBox(height: 4),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 104),
-                              child: Text(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFF47518C),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                              child: isLoading
+                                  ? const SizedBox(
+                                      height: 20,
+                                      width: 20,
+                                      child: CircularProgressIndicator(
+                                        color: Colors.white,
+                                        strokeWidth: 2,
+                                      ),
+                                    )
+                                  : Text(
+                                      'Verify',
+                                      style: GoogleFonts.montserrat(
+                                        textStyle: const TextStyle(
+                                          color: Color(0xFFFAFAFA),
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 14,
+                                          height: 1.5,
+                                        ),
+                                      ),
+                                    ),
+                            ),
+                          ),
+                          SizedBox(height: screenHeight * 0.008),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
                                 'Back To',
                                 style: GoogleFonts.poppins(
                                   textStyle: const TextStyle(
@@ -420,23 +393,10 @@ setState(() {
                                   ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(width: 4),
-                            SizedBox(
-                              width: 61,
-                              height: 28,
-                              child: TextButton(
+                              TextButton(
                                 onPressed: () {
                                   Navigator.pop(context);
-
                                 },
-                                style: TextButton.styleFrom(
-                                  padding: const EdgeInsets.only(left: 0),
-                                  backgroundColor: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                ),
                                 child: Text(
                                   'Sign Up',
                                   style: GoogleFonts.poppins(
@@ -449,13 +409,13 @@ setState(() {
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
